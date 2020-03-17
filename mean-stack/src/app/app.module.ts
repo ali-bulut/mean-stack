caa3059e48b16aa8f6ac3ae91823b1b350cec1d5
Ali Bulut <alibulut@yahoo.com>
2020-03-17 15:56:35 +0300

    routing system has been added to angular side

--- mean-stack/src/app/app.module.ts
+++ mean-stack/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {PostListComponent} from './posts/post-list/post-list.component';
 import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
    declarations: [
@@ -34,6 +35,7 @@ import { HeaderComponent } from './header/header.component';
 
       
       BrowserModule,
+      AppRoutingModule,
       FormsModule,
       BrowserAnimationsModule,
       MatInputModule,
