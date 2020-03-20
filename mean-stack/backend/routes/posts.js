bf43ca16a06249d8b2cb421e2e7dd9a5bb684cb5
Ali Bulut <alibulut@yahoo.com>
2020-03-20 03:24:20 +0300

    authorization operations were done

--- mean-stack/backend/routes/posts.js
+++ mean-stack/backend/routes/posts.js
@@ -68,7 +68,8 @@ router.put('/:id', checkAuth, multer({storage:storage}).single("image"), (req,re
         _id:req.body.id,
         title:req.body.title,
         content:req.body.content,
-        imagePath:imagePath
+        imagePath:imagePath,
+        creator:req.userData.userId
     });
     Post.updateOne({_id: req.params.id, creator:req.userData.userId}, post).then(result=>{
         if(result.nModified > 0){
